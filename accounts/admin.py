bf6f8a762cde4d55c6f67cb47aadcca43a1cbbc8
aryan008 <adam.s.ryan@pwc.com>
2021-09-09 09:10:57 +0000

    got customer page working

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import *
 # Register your models here.
+
+from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Orders)
 admin.site.register(Tag)
+admin.site.register(Order)
